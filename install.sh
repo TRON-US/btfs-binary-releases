2e463cc0a58818345305e01d5d63ad595f67422e
Leo Li <cyberwalker82@live.com>
2020-11-09 17:09:43 +0000

    BTFS-2442 Add fs-repo-migrations binary

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 OS="DEFAULT"
 ARCH="DEFAULT"
-
 while [[ -n "$1" ]]; do
     case "$1" in
         -o)
@@ -28,20 +27,19 @@ while [[ -n "$1" ]]; do
             ;;
     esac
 done
-
 if [[ "${ARCH}" = "DEFAULT" ]]; then
     echo "Please enter operating system. Supported: darwin, linux. darwin means macOS."
     exit 1
 fi
-
 if [[ "${OS}" = "DEFAULT" ]]; then
     echo "Please enter arch. Supported: amd64, 386, arm64, arm"
     exit 1
 fi
 
+mkdir -p /tmp/btfs.tmp && cd /tmp/btfs.tmp
+
 n=0
 while true; do
-
     m=0
     while true; do
         curl -fL -o config_${OS}_${ARCH}_downloaded.yaml https://github.com/TRON-US/go-btfs/releases/latest/download/config_${OS}_${ARCH}.yaml && curl -fL -O https://github.com/TRON-US/go-btfs/releases/latest/download/btfs-${OS}-${ARCH}.tar && break
@@ -71,19 +69,53 @@ while true; do
     sleep 5
 done
 
-rm btfs-${OS}-${ARCH}.tar config_${OS}_${ARCH}_downloaded.yaml
+m=0
+while true; do
+    curl -fL -O https://github.com/TRON-US/btfs-distributions/raw/master/fs-repo-migrations/versions && break
+    let "m++"
+    if [[ "${m}" -ge 3 ]]; then echo "Download of fs-repo-migrations version file failed, confirm your internet connection to GitHub is working and rerun this script."; exit 1; fi
+    sleep 5
+done
+VERSION=$(cat versions | sort -V | tail -n 1)
+
+FILE1="fs-repo-migrations_${VERSION}_${OS}-${ARCH}.tar.gz"
+FILE2="fs-repo-migrations_${VERSION}_${OS}-${ARCH}.tar.gz.sha512"
+URL1="https://github.com/TRON-US/btfs-distributions/raw/master/fs-repo-migrations/${VERSION}/${FILE1}"
+URL2="https://github.com/TRON-US/btfs-distributions/raw/master/fs-repo-migrations/${VERSION}/${FILE2}"
+n=0
+while true; do
+    m=0
+    while true; do
+        curl -fL -O "${URL1}" && curl -fL -O "${URL2}" && break
+        let "m++"
+        if [[ "${m}" -ge 3 ]]; then echo "Download of fs-repo-migrations files failed, confirm your internet connection to GitHub is working and rerun this script."; exit 1; fi
+        sleep 5
+    done
+
+    if ! command -v shasum > /dev/null; then echo "Please install shasum command first, then rerun this script."; exit 1; fi
+    SHA512=$(shasum -a 512 "${FILE1}" | awk '{print $1}')
+    SHA512_FROM_FILE=$(grep "${FILE1}" "${FILE2}" |awk '{print $1}')
+    if [[ "${SHA512}" = "${SHA512_FROM_FILE}" ]]; then break; fi
+
+    let "n++"
+    if [[ "${n}" -ge 3 ]]; then echo "${FILE1} is corrupted, confirm your internet connection to GitHub is working and rerun this script."; exit 1; fi
+    sleep 5
+done
+tar -xzf "${FILE1}"
 
 BTFSPATH=${HOME}/btfs
-
 if [[ -d "${BTFSPATH}" ]]; then
     echo "~/btfs already exists, deleting ~/btfs first..."
-    rm -rf ${BTFSPATH}
+    rm -rf "${BTFSPATH}"
     echo "Deleting ~/btfs succeeded!"
 fi
 
 mkdir -p ${BTFSPATH}/bin
 mv btfs-${OS}-${ARCH} ${BTFSPATH}/bin/btfs
 mv config_${OS}_${ARCH}.yaml ${BTFSPATH}/bin/config.yaml
+mv  fs-repo-migrations/fs-repo-migrations ${BTFSPATH}/bin/
+
+rm -rf /tmp/btfs.tmp
 
 echo "Installation of BTFS succeeded!"
 exit 0
